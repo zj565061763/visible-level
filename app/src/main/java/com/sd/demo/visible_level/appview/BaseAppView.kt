17196f1a745d5aa76a1563151d9a571b674a51bf
zhengjun <565061763@qq.com>
2022-06-08 00:33:45 +0800

    demo

--- app/src/main/java/com/sd/demo/visible_level/appview/BaseAppView.kt
+++ app/src/main/java/com/sd/demo/visible_level/appview/BaseAppView.kt
@@ -1,33 +1,19 @@
-package com.sd.demo.visible_level.appview;
+package com.sd.demo.visible_level.appview
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.widget.FrameLayout;
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.FrameLayout
+import com.sd.demo.visible_level.MainActivity
+import com.sd.lib.vlevel.FVisibleLevelItem
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+open class BaseAppView(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : FrameLayout(context, attrs), FVisibleLevelItem.VisibilityCallback {
 
-import com.sd.demo.visible_level.MainActivity;
-import com.sd.lib.vlevel.FVisibleLevelItem;
-
-public class BaseAppView extends FrameLayout implements FVisibleLevelItem.VisibilityCallback
-{
-    public BaseAppView(@NonNull Context context)
-    {
-        super(context);
-    }
-
-    public BaseAppView(@NonNull Context context, @Nullable AttributeSet attrs)
-    {
-        super(context, attrs);
-    }
-
-    @Override
-    public void onLevelItemVisibilityChanged(boolean visible, FVisibleLevelItem item)
-    {
-        Log.i(MainActivity.TAG, "AppView onLevelItemVisibilityChanged visible:" + visible
-                + " item:" + item.getName()
-                + " view:" + this);
+    override fun onLevelItemVisibilityChanged(visible: Boolean, item: FVisibleLevelItem) {
+        Log.i(MainActivity.TAG,
+            "AppView onLevelItemVisibilityChanged ${item.name} -> $visible $this")
     }
 }
