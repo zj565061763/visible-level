2ad29f15145ca605d461d6bea5bc38c5054198ab
zhengjun <565061763@qq.com>
2022-06-08 04:16:29 +0800

    暴露level

--- lib/src/main/java/com/sd/lib/vlevel/FVisibleLevelItem.kt
+++ lib/src/main/java/com/sd/lib/vlevel/FVisibleLevelItem.kt
@@ -3,13 +3,11 @@ package com.sd.lib.vlevel
 import java.util.*
 
 class FVisibleLevelItem internal constructor(
-    name: String,
-    level: FVisibleLevel,
-) {
     /** Item名称 */
-    val name = name
+    val name: String,
     /** Item所在的等级 */
-    private val _level = level
+    val level: FVisibleLevel,
+) {
     /** 子级 */
     private var _childLevel: FVisibleLevel? = null
     /** 回调 */
@@ -19,18 +17,18 @@ class FVisibleLevelItem internal constructor(
      * Item是否可见
      */
     val isVisible: Boolean
-        get() = _level.isVisible && _level.currentItem == this
+        get() = level.isVisible && level.currentItem == this
 
     /**
      * 设置Item的子等级
      */
-    fun setChildLevel(level: FVisibleLevel?) {
-        require(_level != level) { "child level should not be current level" }
+    fun setChildLevel(childLevel: FVisibleLevel?) {
+        require(level != childLevel) { "child level should not be current level" }
         val old = _childLevel
-        if (old != level) {
-            _childLevel = level
+        if (old != childLevel) {
+            _childLevel = childLevel
             old?.isVisible = false
-            level?.isVisible = isVisible
+            childLevel?.isVisible = isVisible
         }
     }
 
