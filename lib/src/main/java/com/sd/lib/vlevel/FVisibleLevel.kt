c4aee1b1f0aeaf860879d15c2a94b4e1e58dab63
565061763 <565061763@qq.com>
2022-11-16 17:04:23 +0800

    addItems

--- lib/src/main/java/com/sd/lib/vlevel/FVisibleLevel.kt
+++ lib/src/main/java/com/sd/lib/vlevel/FVisibleLevel.kt
@@ -46,10 +46,9 @@ abstract class FVisibleLevel protected constructor() {
      * 添加Item，跳过重复的Item
      */
     fun addItems(items: Array<String>) {
-        if (isRemoved) return
         if (items.isEmpty()) return
-
-        synchronized(this@FVisibleLevel) {
+        synchronized(FVisibleLevel::class.java) {
+            if (isRemoved) return
             for (item in items) {
                 require(item.isNotEmpty()) { "item is empty" }
                 if (_itemHolder.containsKey(item)) continue
