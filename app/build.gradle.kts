1f1ea5aff094938e3d20ad12ff2f6b1ff4651f45
zj565061763 <565061763@qq.com>
2023-03-21 15:19:41 +0800

    upgrade

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace ="com.sd.demo.visible_level"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+        targetSdk = libs.versions.androidTargetSdk.get().toInt()
+        applicationId = "com.sd.demo.visible_level"
+        versionCode = 1
+        versionName  ="1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(project(":lib"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,16 @@ pluginManagement {
         mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
 rootProject.name = "visible-level"
-include ':app'
-include ':lib'
+include(":app")
+include(":lib")
