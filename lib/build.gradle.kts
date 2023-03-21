fb74e180e52ec6a04b0a7f8a4665b2e79979cf31
zj565061763 <565061763@qq.com>
2023-03-21 15:22:44 +0800

    1.2.0-rc01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "visible-level"
-val libVersionName = "1.2.0-beta03"
+val libVersionName = "1.2.0-rc01"
 
 android {
     namespace = "com.sd.lib.vlevel"
